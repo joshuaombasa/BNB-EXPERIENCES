861e6854df19a9c17f6f23efcd22fa3252d05ad1
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-13 23:26:14 +0300

    updated jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import './App.css'
+import data from './data'
+
+console.log(data)
+
 
 import Navbar from "./Navbar"
 import Hero from "./Hero"
@@ -7,18 +11,24 @@ import Card from "./Card"
 
 export default function App() {
   
+const cardHtml = data.map((item) => {
+  return <Card  
+  img={item.coverImg}
+  rating={item.stats.rating} 
+  reviewCount={item.stats.reviewCount}
+  country={item.location}
+  title={item.title}
+  price={item.price}
+  />
+})
+
+console.log(cardHtml)
+
 return (
 <div>
   <Navbar />
   <Hero />
-  <Card 
-      img="katie-zaferes.png"
-      rating="5.0"
-      reviewCount={6}
-      country="USA"
-      title="Life Lessons with Katie Zaferes"
-      price={136}
-  />
+  {cardHtml}
 </div>
 )
 }
