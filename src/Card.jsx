a120bcdedb5a0fde02b01547bc7fffe1d2caac94
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-14 20:31:24 +0300

    updated jsx file

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,18 +1,26 @@
 import React from "react"
 
 export default function Card(props) {
+    console.log(props.item)
+    let badgeText
+    if (props.item.openSpots === 0) {
+        badgeText = 'SOLD OUT'
+    } else if (props.item.location === 'Online') {
+        badgeText = 'ONLINE'
+    }
+
     return (
         <div className="card">
-            {props.openSpots < 1 && <div className="badge">SOLD OUT</div>}
-            <img src={`public/${props.img}`} className="card--image" />
+            {badgeText && <div className="badge">{badgeText}</div>}
+            <img src={`public/${props.item.coverImg}`} className="card--image" />
             <div className="card--stats">
                 <img src="src/assets/star.png" className="card--star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.country}</span>
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p>{props.item.title}</p>
+            <p><span className="bold">From ${props.item.price}</span> / person</p>
         </div>
     )
 }
