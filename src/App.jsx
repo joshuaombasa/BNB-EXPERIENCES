2034f04c15edbfabc3e2495ca6588d017d3a33f3
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-14 12:24:02 +0300

    updated jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,28 @@ import Hero from "./Hero"
 import Card from "./Card"
 
 export default function App() {
-  
-const cardHtml = data.map((item) => {
-  return <Card  
-  img={item.coverImg}
-  rating={item.stats.rating} 
-  reviewCount={item.stats.reviewCount}
-  country={item.location}
-  title={item.title}
-  price={item.price}
-  />
-})
 
-console.log(cardHtml)
+  const cardHtml = data.map((item) => {
+    return <Card
+      key={item.id}
+      img={item.coverImg}
+      rating={item.stats.rating}
+      reviewCount={item.stats.reviewCount}
+      country={item.location}
+      title={item.title}
+      price={item.price}
+    />
+  })
 
-return (
-<div>
-  <Navbar />
-  <Hero />
-  {cardHtml}
-</div>
-)
+  console.log(cardHtml)
+
+  return (
+    <div className='app'>
+      <Navbar />
+      <Hero />
+      <section className='cards-list'>
+        {cardHtml}
+      </section>
+    </div>
+  )
 }
